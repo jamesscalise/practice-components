90b60cd9c06b06111ead1cefa2bcd9985a4efe40
James <jamescscalise@gmail.com>
2020-11-14 21:11:58 -0600

    add second hook

--- src/Sample.js
+++ src/Sample.js
@@ -3,11 +3,12 @@ import React from 'react'
 const Sample = ({title}) => {
 
     const [count, setCount] = React.useState(0)
+    const [name, setName] = React.useState('')
 
     return (
         <div>Here is the sample component! {title}
             <div>Count: {count}</div>
-            <button onClick = {() => setCount(count + 1)}>Click this to increase the count</button>
+            <button onClick = {() =>    setCount(count + 1)}>Click this to increase the count</button>
         </div>
     )
 
