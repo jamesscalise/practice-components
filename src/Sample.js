965e14bd02764133a0e872f1f86bb14f85e0b3bd
James <jamescscalise@gmail.com>
2020-11-14 20:59:39 -0600

    add hook

--- src/Sample.js
+++ src/Sample.js
@@ -1,6 +1,17 @@
-const Sample = ({title}) => (
-<div>Here is the sample component! {title}</div>
+import React from 'react'
+
+const Sample = ({title}) => {
+
+    const [count, setCount] = React.useState(0)
+
+    return (
+        <div>Here is the sample component! {title}
+            <div>Count: {count}</div>
+            <button>Click this to increase the count</button>
+        </div>
+    )
+
     
-)
+}
 
 export default Sample;
